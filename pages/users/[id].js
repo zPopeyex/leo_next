f61497d7703ad023bb4ef56fd3c3bf369a3591b8
zPopeyex <stven-457@hotmail.com>
2022-06-03 17:39:06 -0500

    add static content and style

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -12,16 +12,58 @@ export default function user() {
         <Title>User Details</Title>
         <p>User ID: {router.query.id}</p>
       </Layout>
-      <style jsx global>
+      <style jsx>
         {`
-          p {
-            color: darkgray;
+          nav {
+            padding-top: 10px;
           }
-          p:hover {
-            color: pink;
+          a {
+            padding: 0 14px;
           }
         `}
+      </style>
+      <style jsx>
+          {`
+            .grid {
+              display: flex;
+              flex-wrap: wrap;
+              max-width: 800px;
+              margin-top: 2rem;
+            }
+            .card {
+              margin: 1rem;
+              flex-basis: 45%;
+              padding: 1.5rem;
+              color: black;
+              text-decoration: none;
+              border: 2px solid #eaeaea;
+              border-radius: 10px;
+              transition: color 0.15s ease, border-color 0.15s ease;
+            }
+            .card:hover,
+            .card:focus
+            .card:active{
+              color: #0070f3;
+            }
+            .card h3 {
+              margin . 0 0 1rem 0;
+              front-size: 1.25rem;
+            }
+            
+            `}
+
       </style>
     </div>
   );
 }
+
+export async function getStaticProps({params}){
+      const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+      const user = res.json();
+
+      return{
+        props:{
+          user
+        }
+      }
+}   
--- pages/users/index.js
+++ pages/users/index.js
@@ -1,11 +1,31 @@
 import Navbar from "../../components/navbar";
 import Title from "../../components/title";
 import Layout from "../../components/layout";
+import Link from "next/link";
 
-export default function users() {
+export default function users({users}) {
+
   return (
     <Layout>
       <Title>Users Page</Title>
+      <div className='grid'>
+        {users.map(user => {
+          return(
+            <Link href={'/users/[id]'} as={`/users/${user.id}`}>
+
+              <a className='card'>
+                <h3>User</h3>
+                <p>Name: {user.name}</p>
+                <p>Email: {user.email}</p>
+                
+
+              </a>
+
+            </Link>
+          )
+        })}   
+
+      </div>
       <p>This is the users page</p>
       <style jsx>
         {`
@@ -29,6 +49,49 @@ export default function users() {
 
         `}
       </style>
+      <style jsx>
+          {`
+            .grid {
+              display: flex;
+              flex-wrap: wrap;
+              max-width: 800px;
+              margin-top: 2rem;
+            }
+            .card {
+              margin: 1rem;
+              flex-basis: 45%;
+              padding: 1.5rem;
+              color: black;
+              text-decoration: none;
+              border: 2px solid #eaeaea;
+              border-radius: 10px;
+              transition: color 0.15s ease, border-color 0.15s ease;
+            }
+            .card:hover,
+            .card:focus
+            .card:active{
+              color: #0070f3;
+            }
+            .card h3 {
+              margin . 0 0 1rem 0;
+              front-size: 1.25rem;
+            }
+            
+            `}
+
+      </style>
     </Layout>
   );
+}
+
+export async function getStaticProps(){
+  const res = await fetch ('https://jsonplaceholder.typicode.com/users');
+  const users = await res.json();
+
+  return{
+    props:{
+      users
+    }
+  }
+
 }
